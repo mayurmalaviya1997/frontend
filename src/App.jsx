107fb9e31d13693cc72d2056a378ab5b2d650d5d
Mayur Malaviya <mmalaviya517@gmail.com>
2023-06-19 22:40:50 +0530

    master commint-1

--- src/App.jsx
+++ src/App.jsx
@@ -8,6 +8,7 @@ import Cart from './component/Cart'
 const App = () => {
   return (
     <div>
+      <h1> chnages in master branch </h1>
         <Routes>
           <Route path="/" element={  <Home/>} />
           <Route path="/cart" element={  <Cart/>} />
